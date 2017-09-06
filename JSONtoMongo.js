396e1a3bce53630001b4e5be4e89976d8e8375fd
Megan Quach <mquach95@gmail.com>
2017-09-06 19:09:28 -0400

    update

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,7 +10,7 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
-mongoose.connect(config.db.uri);
+mongoose.connect('mongodb://mquach:test123@ds053196.mlab.com:53196/mquach_cen3031');
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
@@ -22,7 +22,7 @@ fs.readFile('listings.json','utf8', function(err, data) {
         throw err;
     listingData = JSON.parse(data).entries;
     
-    for(var i = 0; i < listingData.length; i++) {
+    for(var i = 0;i<listingData.length;i++) {
         var buildingData = new Listing(listingData[i]);
         buildingData.save(function(err) {
             if(err)
