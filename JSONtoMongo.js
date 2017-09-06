73ac22dd1840fafee3406198be03b327421d6056
Megan Quach <mquach95@gmail.com>
2017-09-06 16:10:28 -0400

    update files

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,12 +10,27 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
-
+mongoose.connect(config.db.uri);
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+var fs = require('fs');
+var listingData;
+fs.readFile('listings.json','utf8', function(error, data) {
+    if (error)
+        throw error;
+    listingData = JSON.parse(data).entries;
+    
+    for(var i = 0;i<listingData.length;i++) {
+        var buildingData = new Listing(listingData[i]);
+        buildingData.save(function(err) {
+            if(error)
+                throw error;
+        });
+    }
+    
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -5,6 +5,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+    Listing.findOne({ code: 'LBW' }, function(error, listing) {
+        if(error)
+            throw error;
+        console.log(listing);
+    });
 };
 var removeCable = function() {
   /*
@@ -12,17 +17,34 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+    Listing.findOneandRemove({ code: 'CABL'}, function(error, listing) {
+        if(error)
+            throw error;
+        console.log(listing);
+    });
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+    Listing.findOneAndUpdate({ code: 'PHL', {address'Phelps Lab, Gainesville, FL, 32608'}}, function(error, listing) {
+        if (error)
+            throw error;
+        console.log(listing)
+    });
 };
+
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+    Listing.find({}, function(error, listing) {
+        if (error)
+            throw error;
+        console.log(listing);
+    });
+    
 };
 
 findLibraryWest();
